d86618b05e70d2260cb9df8df20a1d9018d8e6f3
ParkYoungWoong <thesecon@gmail.com>
2019-10-23 06:05:46 +0900

    Add eslint for vue

---
+++ 0_config/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: "babel-eslint",
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended"
+  ],
+  plugins: [
+    "vue"
+  ],
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
