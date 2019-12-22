54c35faad9ad3dc71dfd2594418c4eb32181fad9
ParkYoungWoong <thesecon@gmail.com>
2019-12-22 15:24:41 +0900

    Add comment for '.eslintrc.js'

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,19 @@ module.exports = {
   root: true,
   parserOptions: {
     parser: 'babel-eslint',
-    ecmaVersion: 2015,
+    ecmaVersion: 2015, // same as 6
     sourceType: 'module'
   },
+  // 사전에 정의된 전역 변수 설정
   env: {
     browser: true,
     node: true
   },
+  // 사용 가능한 규칙 세트 확장
   extends: [
     // https://github.com/standard/eslint-config-standard
     'standard',
+
     // https://eslint.vuejs.org/rules/
     // 'plugin:vue/base'
     'plugin:vue/essential'
@@ -22,7 +25,7 @@ module.exports = {
     'vue'
   ],
   rules: {
-    // 예외 규칙을 추가할 수 있습니다
+    // 다음과 같은 예외 규칙을 추가할 수 있습니다
     'vue/html-self-closing': ['error', {
       'html': {
         'void': 'always',
